a05e33125bb686d60c257b5ea5ebb8106ca5ad88
tracek <lukasz.tracewski@outlook.com>
2019-02-24 14:54:43 +0100

    add test env

---
+++ Dockerfile
@@ -0,0 +1,20 @@
+FROM continuumio/anaconda3:2018.12
+COPY . /app
+WORKDIR /app
+RUN conda install -c conda-forge --quiet --yes \
+    Python=3.6.8 \
+    dash=0.37.0 \
+    dash-core-components=0.43.1 \
+    dash-html-components=0.13.5 \
+    dash-table=3.4.0 \
+    plotly=3.6.1 \
+    aubio=0.4.9 \
+    yaafe=0.70 \
+    librosa=0.6.3 \
+    pandas=0.24.1 \
+    numpy=1.16.1 \
+    scipy=1.2.1 \
+    boto3=1.9.101
+
+EXPOSE 8080
+CMD ["python", "application.py"]
---
+++ application.py
@@ -0,0 +1,35 @@
+import os
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+application = app.server
+
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+
+if __name__ == '__main__':
+    application.run(debug=False, port=8080)
