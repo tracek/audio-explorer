d6d5d4c99972fa01fdb2e322901cc61882e8f630
tracek <lukasz.tracewski@outlook.com>
2019-03-23 11:43:35 +0100

    audio cli

--- audiocli.py
+++ audiocli.py
@@ -22,32 +22,38 @@ def root(quiet):
 
 
 @root.command('a2f', help='Audio to HDF5 features')
-@click.option("--path", "-i", type=click.STRING, required=True, help="Path to audio.")
-@click.option("--output", "-o", type=click.STRING, default='.', help="Output file or directory.")
+@click.option("--input", "-in", type=click.STRING, required=True, help="Path to audio.")
+@click.option("--output", "-out", type=click.STRING, default='.', help="Output file or directory.")
 @click.option("--jobs", "-j", type=click.INT, default=1, help="Number of jobs to run", show_default=True)
 @click.option("--config", "-c", type=click.Path(exists=True), default='audioexplorer/algo_config.ini', help="Feature extractor config.")
 @click.option('--single', is_flag=True, help='Produce a single HDF5')
-def process(path, output, jobs, config, single):
-    if not os.path.exists(output):
-        os.makedirs(output)
-    shutil.copy(config, output)
+def process(input, output, jobs, config, single):
     extractor_config = configparser.ConfigParser()
     extractor_config.read(config)
-    audio_files = glob.glob(path + '/*.wav', recursive=True)
-    for path in audio_files:
-        logging.info(f'Processing {path}')
-        y, sr = librosa.load(path, sr=16000)
-        filename_noext = os.path.splitext(os.path.basename(path))[0]
+    audio_files = glob.glob(input + '/*.wav', recursive=False)
+    config_signature = get_name_from_config(config)
+    
+    if single:
+        os.makedirs(output, exist_ok=True)
+        output_path = os.path.join(output, config_signature + '.h5')
+        shutil.copy(config, output)
+    else:
+        output_path = os.path.join(output, config_signature)
+        os.makedirs(output_path, exist_ok=True)
+        shutil.copy(config, output_path)
+    
+    for wav in audio_files:
+        logging.info(f'Processing {wav}')
+        y, sr = librosa.load(wav, sr=16000)
+        filename_noext = os.path.splitext(os.path.basename(wav))[0]
         key = filename_noext.replace('-', '_')
         feats = features.get(y, sr, n_jobs=jobs, **extractor_config)
 
         if single:
-            name = get_name_from_config(config)
-            output_path = os.path.join(output, name + '.h5')
             feats.to_hdf(output_path, key=key, mode='a', format='fixed')
         else:
-            output_path = os.path.join(output, filename_noext + '.h5')
-            feats.to_hdf(output_path, key=key, mode='w', format='fixed')
+            output_path_file = os.path.join(output_path, filename_noext + '.h5')
+            feats.to_hdf(output_path_file, key=key, mode='w', format='fixed')
 
 
 def get_name_from_config(configpath):
@@ -60,22 +66,25 @@ def get_name_from_config(configpath):
 
 
 @root.command('f2m', help='Features to embedding model')
-@click.option("--features", "-i", type=click.STRING, help="Path to h5 features.")
-@click.option("--config", "-c", type=click.Path(exists=False),
-              default='algo_config.ini', help="Feature extractor config.")
-@click.option("--embedding", "-e", type=click.Choice(['tsne', 'umap'], case_sensitive=False),
+@click.option("--input", "-in", type=click.STRING, help="Path to h5 features.", required=True)
+@click.option("--output", "-out", help="Model output path.")
+@click.option("--algo", "-a", type=click.Choice(['tsne', 'umap'], case_sensitive=False),
               default='tsne', help='Embedding to use')
-def h5_to_embedding(features, config, embedding):
-    h5_features = glob.glob(features + '/*.h5', recursive=True)
-    dfs = [pd.read_hdf(path) for path in h5_features]
-    dfs = pd.concat(dfs)
-    if not os.path.exists(config):
-        name = 'audio_embedding'
-        logging.info(f'No feature config provided or found. The output will default to {name}')
+def h5_to_embedding(input, output, algo):
+    if os.path.isfile(input):
+        dfs = pd.read_hdf(input)
+        if not output:
+            output = os.path.splitext(input)[0]
+    elif os.path.isdir(input):
+        input = os.path.normpath(input)
+        h5_features = glob.glob(input + '/*.h5', recursive=False)
+        dfs = [pd.read_hdf(path) for path in h5_features]
+        dfs = pd.concat(dfs)
+        if not output:
+            output = input
     else:
-        name = get_name_from_config(config)
-
-    embedding.fit_and_dump(dfs.values, embedding=embedding, name=name)
+        raise Exception(f'Input {input} not recognised as file or directory.')
+    embedding.fit_and_dump(dfs.values, embedding=algo, name=output)
 
 
 @root.command('m2e', help='Model to embedddings')
