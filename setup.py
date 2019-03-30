deb676c33d9ba9ffbc0970adc731685b09757c01
tracek <lukasz.tracewski@outlook.com>
2019-03-30 15:55:21 +0100

    add setup script

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='audiocli',
+    version='0.1',
+    py_modules=['audi-explorer'],
+    install_requires=[
+        'Click',
+    ],
+    entry_points='''
+        [console_scripts]
+        audiocli=audiocli:root 
+    ''',
+)
