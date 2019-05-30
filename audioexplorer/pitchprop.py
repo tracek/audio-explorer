ab72ea4ec0fb3e154d3de1523751e29dc2393831
tracek <lukasz.tracewski@gmail.com>
2019-05-30 08:37:07 +0200

    improve pitch detection

--- audioexplorer/pitchprop.py
+++ audioexplorer/pitchprop.py
@@ -20,7 +20,7 @@ import numpy as np
 import pandas as pd
 
 
-def get_pitch_stats(signal: np.ndarray, fs: int, block_size: int, hop: int, tolerance: float = 0.2,
+def get_pitch_stats(signal: np.ndarray, fs: int, block_size: int, hop: int, tolerance: float = 0.8,
                     algorithm = 'yinfft') -> dict:
     """
     Get basic statistic on pitch in the given signal
@@ -39,28 +39,33 @@ def get_pitch_stats(signal: np.ndarray, fs: int, block_size: int, hop: int, tole
     pitch_array = []
     for frame in signal_win[:-1]:
         pitch = pitch_o(frame)[0]
-        confidence = pitch_o.get_confidence()
-        if confidence > tolerance:
+        if pitch > 0:
             pitch_array.append(pitch)
 
     if pitch_array:
         pitch_array = np.array(pitch_array)
-        Q25, Q50, Q75 = np.quantile(pitch_array, [0.25, 0.5, 0.75])
+        Q25, Q50, Q75 = np.quantile(pitch_array, [0.25, 0.50, 0.75])
         IQR = Q75 - Q25
         median = np.median(pitch_array)
+        pitch_min = pitch_array.min()
+        pitch_max = pitch_array.max()
     else:
         Q25 = 0
         Q50 = 0
         Q75 = 0
         median = 0
         IQR = 0
+        pitch_min = 0
+        pitch_max = 0
 
     pitchstats = {
-        'pitch_mean': Q50,
         'pitch_median': median,
+        'pitch_mean': Q50,
         'pitch_Q25': Q25,
         'pitch_Q75': Q75,
-        'pitch_IQR': IQR
+        'pitch_IQR': IQR,
+        'pitch_min': pitch_min,
+        'pitch_max': pitch_max
     }
 
     return pitchstats
